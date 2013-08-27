d687d593153fc56617df9b5d8916296ec47f8ceb
poppinyunhai <yiranyunhaisrh@gmail.com>
2013-08-27 14:58:01 +0800

    yanzheng
    yanzheng

--- app/models/meet.rb
+++ app/models/meet.rb
@@ -1,5 +1,7 @@
 class Meet < ActiveRecord::Base
   attr_accessible :company, :name, :phone, :statu_name, :qq, :statu_id
   belongs_to :statu
-  validates_uniqueness_of :name, :message => "111"
+  validates_uniqueness_of :name
+  validates_numericality_of :phone
+  validates_numericality_of :qq
 end
